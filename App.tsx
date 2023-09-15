be8c82424d53d2f8ea7fbe28805cd24f3c5810da
Michael Clapham <michaeljclapham@gmail.com>
2023-09-15 15:15:12 +0100

    Add react native maps (+web)

--- App.tsx
+++ App.tsx
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 51.53487,
+          longitude: -0.1233946,
+          latitudeDelta: 0.1,
+          longitudeDelta: 0.1
+        }}
+      ></MapView>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +26,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
   },
 });
