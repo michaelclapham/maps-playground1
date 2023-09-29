a3df0e465566908104ed3cb179d508be7fc652f7
Michael Clapham <michael.clapham@guardian.co.uk>
2023-09-29 15:58:54 +0100

    Add react leaflet

--- rnm-leaflet/index.tsx
+++ rnm-leaflet/index.tsx
@@ -1,8 +1,24 @@
-import type {MapViewProps} from "react-native-maps";
+import type { MapViewProps } from "react-native-maps";
+import { MapContainer, TileLayer, useMap } from "react-leaflet";
 
 export default function MapView(props: MapViewProps) {
-    return <div>
+    const lat = props.initialRegion?.latitude ?? 54
+    const long = props.initialRegion?.longitude ?? 1
+    return (
+        <div style={{width: "100%", height: "90%"}}>
         <p>lat is {props.initialRegion?.latitude}</p>
         <p>long is {props.initialRegion?.longitude}</p>
-    </div>
+            <MapContainer
+                center={[lat, long]}
+                zoom={13}
+                scrollWheelZoom={false}
+                style={{width: "100%", height: "100%"}}
+            >
+                <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+            </MapContainer>
+        </div>
+    );
 }
